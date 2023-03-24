af8ca985cc9812dcf0881277c94e281c68b56db1
Gabor Szabo <gabor.attila.szabo@gmail.com>
2023-03-24 06:44:38 -0700

    final script

--- 003_a_pivotal_moment/003.R
+++ 003_a_pivotal_moment/003.R
@@ -1,60 +1,61 @@
+# WEEK 003: A PIVOTAL MOMENT
+
+# 0. LOADING PACKAGES ----
+library(tidyverse)
+library(sherlock)
 
 
-bond_strength <- load_file(
-    path = "https://raw.githubusercontent.com/gaboraszabo/datasets-for-sherlock/main/bond_strength_multivari.csv", 
-    filetype = ".csv")
+# 1. READ IN DATA ----
 
-
-bond_strength %>% View()
-View(bond_strength)
-
-# 
-bond_strength %>% glimpse()
-glimpse(bond_strength)
-
-
-bond_strength %>% 
-    head()
+bond_strength_wide <- load_file("https://raw.githubusercontent.com/gaboraszabo/datasets-for-sherlock/main/bond_strength_wide.csv", 
+                                filetype = ".csv")
 
 
 
+# 2. DATA TRANSFORMATION ----
+
+bond_strength_long <- bond_strength_wide %>% 
+    
+    # 2.1 Create "Cycle" column ---- 
+mutate(Cycle = rep(1:3, times = 3) %>% as_factor()) %>% 
+    
+    # 2.2 Convert to long form ----
+pivot_longer(cols = 2:6, names_to = "Line", values_to = "Bond_Strength") %>% 
+    
+    # 2.3 Remove "Line" string ----
+mutate(Line = Line %>% str_remove("Line ")) %>% 
+    
+    # 2.4 Arrange by Time and Line variables (not absolutely necessary) ----
+arrange(Time, Line)
+
+
+bond_strength_long
+
+
+
+# 3. PLOT DATA ----
+
+# 3.1 STRATIFICATION BY LINE ----
+bond_strength_long %>% 
+    # calling the ggplot() function and creating a blank "canvas" (coordinate system)
+    ggplot(aes(x = Line, y = Bond_Strength)) + 
+    # adding a geom (visual)
+    geom_point(size = 3.5, color = "darkblue", alpha = 0.3) +
+    # adding a custom theme
+    theme_sherlock() +
+    # customizing labels
+    labs(title = "Bond Strength Characterization Study", 
+         y     = "Bond Strength [lbf]")
+
+
+
+# 3.2 STRATIFICATION BY LINE USING DRAW_CATEGORICAL_SCATTERPLOT() FUNCTION ----
+bond_strength_long %>% 
+    draw_categorical_scatterplot(y_var = Bond_Strength, grouping_var_1 = Line, plot_means = TRUE, size = 3.5)
 
 
 
 
-bond_strength$Bond_Strength
-
-bond_strength$Bond_Strength[1]
-
-
-# 4. PLOT 
-bond_strength
 
 
 
-bond_strength %>% 
-    draw_categorical_scatterplot(y_var = `Bond Strength`)
-
-bond_strength %>% 
-    draw_categorical_scatterplot(y_var = `Bond Strength`, 
-                                 grouping_var_1 = `Bonding Station`,
-                                 plot_means     = TRUE)
-bond_strength %>% 
-    draw_categorical_scatterplot(y_var = `Bond Strength`, 
-                                 grouping_var_1 = `Bonding Station`, 
-                                 grouping_var_2 = `Stopcock Mold Cavity Number`, 
-                                 size = 3,
-                                 jitter = FALSE)
-
-
-
-
-
-bond_strength %>% 
-    group_by(Line) %>% 
-    summarize(mean_bond_stregnth = mean(Bond_Strength))
-
-
-bond_strength %>% 
-    mutate(Bond_Strength_N = Bond_Strength * 4.44822) %>% 
-    select(index, Bond_Strength, Bond_Strength_N, everything())
