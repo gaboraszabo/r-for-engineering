6a7992a6a8a93a5a514aa5d1e21d9fd3b2ffbf14
Gabor Szabo <gabor.attila.szabo@gmail.com>
2023-03-30 06:55:49 -0700

    fix

--- 004_small_multiples_huge_advantage/004.R
+++ 004_small_multiples_huge_advantage/004.R
@@ -1,4 +1,4 @@
-
+# WEEK 004: SMALL MULTIPLES, HUGE ADVANTAGE
 
 # 0. LOAD LIBRARIES ----
 library(tidyverse)
@@ -6,12 +6,10 @@ library(sherlock)
 
 
 # 1. READ IN DATA ----
-
 bond_strength_wide <- load_file("https://raw.githubusercontent.com/gaboraszabo/datasets-for-sherlock/main/bond_strength_wide.csv", 
                                 filetype = ".csv")
 
 # 2. DATA TRANSFORMATION ----
-
 bond_strength_long <- bond_strength_wide %>%
     mutate(Cycle = rep(1:3, times = 3) %>% as_factor()) %>% 
     pivot_longer(cols = 2:6, names_to = "Line", values_to = "Bond_Strength") %>% 
@@ -19,19 +17,21 @@ bond_strength_long <- bond_strength_wide %>%
     arrange(Time, Line)
 
 
-# 3. 
+# 3. PLOTTING ----
 
+# 3.1 LINE PLOT - FIRST ATTEMPT ----
 bond_strength_long %>% 
     ggplot(aes(Cycle, Bond_Strength)) +
     geom_line() +
     theme_sherlock()
 
+# 3.2 LINE PLOT - SECOND ATTEMPT ----
 bond_strength_long %>% 
     ggplot(aes(Cycle, Bond_Strength, group = Line)) +
     geom_line() +
     theme_sherlock()
 
-
+# 3.3 LINE PLOT WITH FACET_WRAP() ----
 bond_strength_long %>% 
     ggplot(aes(Cycle, Bond_Strength, group = Line, color = Line)) +
     geom_line() +
@@ -40,21 +40,13 @@ bond_strength_long %>%
 
 
 bond_strength_long %>% 
-    ggplot(aes(Cycle, Bond_Strength, group = Line, color = Line)) +
-    geom_line() +
-    facet_wrap(~ Time, nrow = 1) +
-    theme_sherlock()
-
- 
-
-   
-bond_strength_long %>% 
     ggplot(aes(Cycle, Bond_Strength, group = Time, color = Time)) +
     geom_line() +
     facet_wrap(~ Line, nrow = 1) +
     theme_sherlock()
 
 
+# 3.4 FACET_GRID() ----
 bond_strength_long %>% 
     ggplot(aes(Cycle, Bond_Strength, group = Time, color = Time)) +
     geom_line() +
